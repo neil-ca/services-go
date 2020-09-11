13287319a4bc2e09cc6dc34b136306f4a26bd085
ulises <ulisescamps9@gmail.com>
2020-09-11 15:28:50 -0500

    grpc

---
+++ protobufs/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	pb "github.com/Neil-Uli/rest/protofiles"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	p := &pb.Person{
+		Id:    1234,
+		Name:  "Roger F",
+		Email: "rf@example.com",
+		Phones: []*pb.Person_PhoneNumber{
+			{Number: "555-4321", Type: pb.Person_HOME},
+		},
+	}
+	p1 := &pb.Person{}
+	body, _ := proto.Marshal(p)
+	_ = proto.Unmarshal(body, p1)
+	fmt.Println("Original struct loaded from proto file:", p, "\n")
+	fmt.Println("Marshaled proto data: ", body, "\n")
+	fmt.Println("Unmarshaled struct: ", p1)
+}
