d12dddb7c2f10ca0e31a45103540a5f3be604ab2
ulises <ulisescamps9@gmail.com>
2020-09-13 22:47:12 -0500

    urlshortener

---
+++ urlshortener/models/models.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func InitDB() (*sql.DB, error) {
+	var err error
+	db, err := sql.Open("postgres", "postgres://neil:secret@localhost/mydb?sslmode=disable")
+	if err != nil {
+		return nil, err
+	} else {
+		// Create model for our URL service
+		stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS WEB_URL(ID SERIAL PRIMARY KEY, URL TEXT NOT NULL);")
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		res, err := stmt.Exec()
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		return db, nil
+	}
+}
