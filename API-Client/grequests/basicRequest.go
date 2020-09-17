66b402781711280d2272e6a213bcadad81e90bfd
ulises <ulisescamps9@gmail.com>
2020-09-17 00:00:15 -0500

    grequests

---
+++ API-Client/grequests/basicRequest.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/levigross/grequests"
+	"log"
+)
+
+func main() {
+	resp, err := grequests.Get("http://httpbin.org/get", nil)
+	if err != nil {
+		log.Fatalln("Unable to make request: ", err)
+	}
+	log.Println(resp.String())
+}
