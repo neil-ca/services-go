59a528844599929742e993049c7a753cb9b503f4
ulises <ulisescamps9@gmail.com>
2020-09-19 03:28:37 -0500

    json-utils

--- encryptService/helpers/jsonutils.go
+++ encryptService/helpers/jsonutils.go
@@ -1,1 +1,16 @@
 package helpers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+// DecodeEncryptRequest fills struct from JSON details of request
+func DecodeEncryptRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request EncryptRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
