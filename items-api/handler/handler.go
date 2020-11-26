892627faceac3bce264b7bc8071efa80137ff1ca
ulises <ulisescamps9@gmail.com>
2020-11-25 22:09:42 -0600

    adding the remaining methods

--- items-api/handler/handler.go
+++ items-api/handler/handler.go
@@ -17,3 +17,15 @@ func NewHandler(db db.Database) http.Handler {
 	router.Route("/items", items)
 	return router
 }
+
+func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(405)
+	render.Render(w, r, ErrMethodNotAllowed)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+	render.Render(w, r, ErrNotFound)
+}
