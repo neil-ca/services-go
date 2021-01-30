dc1a9a1fc857af027125c4b59e60d75530f2e5ee
ulises <ulisescamps9@gmail.com>
2021-01-29 20:38:46 -0600

    operations with mysql and templates
    Signed-off-by: ulises <ulisescamps9@gmail.com>

--- employee/main.go
+++ employee/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Employee struct will represent our database
 type Employee struct {
-	Id   int
+	ID   int
 	Name string
 	City string
 }
@@ -26,9 +28,141 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-var templ = template.Must(template.ParseGlob("form/*"))
+var tmpl = template.Must(template.ParseGlob("form/*"))
 
+// Index it returns all the records and will write them in a template
 func Index(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
-	query, err := db.Query("SELECT * FROM")
+
+	query, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emp := Employee{}
+	res := []Employee{}
+	for query.Next() {
+		var id int
+		var name, city string
+		err = query.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.ID = id
+		emp.Name = name
+		emp.City = city
+		res = append(res, emp)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+// Show return record with the id required
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nID := r.URL.Query().Get("id")
+	query, err := db.Query("SELECT * FROM Employee WHERE id=?", nID)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	for query.Next() {
+		var id int
+		var name, city string
+		err = query.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.ID = id
+		emp.Name = name
+		emp.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nID := r.URL.Query().Get("id")
+	query, err := db.Query("SELECT * FROM Employee WHERE id=?", nID)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	for query.Next() {
+		var id int
+		var name, city string
+		err = query.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.ID = id
+		emp.Name = name
+		emp.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Edit", emp)
+	defer db.Close()
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		insForm, err := db.Prepare("INSERT INTO Employee(name, city) VALUES(?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city)
+		log.Println("INSERT: Name: " + name + " | City: " + city)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE Employee SET name=?, city=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city, id)
+		log.Println("UPDATE: Name: " + name + " | City: " + city)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+	delForm, err := db.Prepare("DELETE FROM Employee WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(emp)
+	log.Println("DELETE")
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func main() {
+	log.Println("Server started on: http://localhost:8080")
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/new", New)
+	http.HandleFunc("/edit", Edit)
+	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
 }
